16908aa318ac16d3bfe316a7c6a067d87d10f8f2
Philip Condie <philipcondie@Philips-MacBook-Pro.local>
2026-02-01 09:38:14 +0800

    frontend config updates

--- frontend/src/services/api/api.ts
+++ frontend/src/services/api/api.ts
@@ -1,6 +1,6 @@
 import { type CreateScoreRequest, type Score} from "./types";
 
-const API_BASE_URL = 'http://localhost:8000'
+const API_BASE_URL = import.meta.env.VITE_API_URL
 
 export class ApiError extends Error {
     status: number;
